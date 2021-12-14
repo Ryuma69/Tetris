92533ea425cb8d429d0abf5d8937b33955b23338
Hanybay <hanybayazid11@gmail.com>
2021-12-14 19:42:57 +0100

    make

--- include/save.h
+++ include/save.h
@@ -4,8 +4,8 @@
 #include"matrice.h"
 
 void save_score(int *score);
-void save_partie(terrain * t,char * chemin,int * score);
-void charger_partie(terrain * t,char * chemin,int * score);
+void save_partie(terrain * t,char * chemin,int * score,char * nom);
+void charger_partie(terrain * t,char * chemin,int * score,char * nom);
 int * lecture_score(int tab[]);
 void viderBuffer();
 
